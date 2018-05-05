3ac1e9b936e92a68768f48e129b497fc585d9f04
simransdsu <simran5590@gmail.com>
2018-05-04 22:57:23 -0700

    1-1-9 Pattern Matching - where

---
+++ 1-1-9 Pattern Matching - where.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let numbers = [1,2,3,4,5,6,7,8,9]
+
+for number in numbers where number % 2 == 0 {
+    print("\(number) is even")
+}
