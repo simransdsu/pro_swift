196bf57659d99ba7d44b9f96fbf8d21e534cf8e8
simransdsu <simran5590@gmail.com>
2018-05-10 14:36:54 -0700

    2-1-1 Repating Values

---
+++ 2-1-1 Repating Values.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+let heading = "This is a heading"
+let underline = String(repeating: "=", count: heading.count)
+
+
+// 2D String Array
+let equals = [String](repeating: "=", count: heading.count)
+
