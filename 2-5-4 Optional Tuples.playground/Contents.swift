8af8ed682ab1290548a8dd0e1b109574aa5858c2
simransdsu <simran5590@gmail.com>
2018-05-16 20:59:05 -0700

    2-5-4 Tuples Optionals

---
+++ 2-5-4 Optional Tuples.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let optionalElements: (String?, String?) = ("Taylor", nil)
+let optionalTuple: (String, String)? = ("Taylor", "Swift")
+
